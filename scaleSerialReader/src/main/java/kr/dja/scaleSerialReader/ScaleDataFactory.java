6d3fa43b873184ac2a67b326c3253d1925dfb99e
dja12123 <dja12123@naver.com>
2021-07-07 20:41:17 +0900

    정답 업데이트

--- scaleSerialReader/src/main/java/kr/dja/scaleSerialReader/ScaleDataFactory.java
+++ scaleSerialReader/src/main/java/kr/dja/scaleSerialReader/ScaleDataFactory.java
@@ -13,7 +13,30 @@ public class ScaleDataFactory implements ISerialDTOFactory
 	{
 		ScaleDTO dto = new ScaleDTO();
 		
-		//문자열에서 각 필드를 추출하여 dto에 삽입
+		try
+		{
+			dto.setErrorCode(msg.substring(0, 2));
+			
+			if(msg.length() > 2)
+			{
+				dto.setScaleStatus(msg.substring(2, 4));
+				dto.setIdentNo(Integer.parseInt(msg.substring(17, 21).replace("_", "")));
+				dto.setScaleNo(Integer.parseInt(msg.substring(21, 22).replace("_", "")));
+				dto.setGross(Double.parseDouble(msg.substring(22, 30).replace("_", "")));
+				dto.setTare(Double.parseDouble(msg.substring(30, 38).replace("_", "")));
+				dto.setNet(Double.parseDouble(msg.substring(38, 46).replace("_", "")));
+				dto.setUnit(msg.substring(46, 48).replace("_", ""));
+				dto.setTairCode(msg.substring(48, 50));
+				dto.setWeighingRange(msg.substring(50, 51));
+				dto.setTerminalNo(msg.substring(51, 54));
+			}
+
+		}
+		catch(Exception e)
+		{
+			dto = null;
+			log.warn("parse error", e);
+		}
 		
 		return dto;
 	}
--- scaleSerialReader/src/main/java/kr/dja/scaleSerialReader/SerialMessageReaderImpl.java
+++ scaleSerialReader/src/main/java/kr/dja/scaleSerialReader/SerialMessageReaderImpl.java
@@ -1,25 +1,92 @@
 package kr.dja.scaleSerialReader;
 
-public class SerialMessageReaderImpl implements ISerialMessageReader
-{// 이 클래스를 구현할 것
+import java.util.LinkedList;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import lombok.extern.slf4j.Slf4j;
 
-	public SerialMessageReaderImpl(ScaleDataFactory scaleDataFactory)
+@Slf4j
+public class SerialMessageReaderImpl implements ISerialMessageReader
+{
+
+	private ISerialDataObserver observer;
+	private final LinkedList<Byte> byteBuffer;
+	private int recvEndFlag = 0;
+	private final ExecutorService executor;
+	private final ISerialDTOFactory dataFactory;
+	
+	public SerialMessageReaderImpl(ISerialDTOFactory dataFactory)
 	{
-		// TODO Auto-generated constructor stub
+		this.byteBuffer = new LinkedList<Byte>();
+		this.dataFactory = dataFactory;
+		this.executor = Executors.newSingleThreadExecutor();
 	}
 
 	@Override
 	public void readData(byte[] serialData)
 	{
-		// TODO Auto-generated method stub
 
+		for (int index = 0; index < serialData.length; ++index)
+		{
+			byte b = serialData[index];
+			if (this.byteBuffer.size() > 1024) this.byteBuffer.removeFirst();
+			
+			this.byteBuffer.add(b);
+			switch (b)
+			{
+			case '>':
+				this.recvEndFlag = 1;
+				break;
+			case '\r':
+				if (this.recvEndFlag == 1)
+					this.recvEndFlag = 2;
+				else
+					this.recvEndFlag = 0;
+				break;
+			case '\n':
+				if (this.recvEndFlag == 2)
+					this.recvEndFlag = 3;
+				else
+					this.recvEndFlag = 0;
+				break;
+			default:
+				this.recvEndFlag = 0;
+			}
+			if (this.recvEndFlag == 3)
+			{
+				this.recvEndFlag = 0;
+				this.byteBuffer.pollLast();
+				this.byteBuffer.pollLast();
+				this.byteBuffer.pollLast();
+				byte dataChar;
+				StringBuilder sb = new StringBuilder();
+				while (true)
+				{
+					if(this.byteBuffer.isEmpty()) break;
+					dataChar = this.byteBuffer.pollLast();
+					if (dataChar == '<') break;
+					sb.append((char) dataChar);
+				}
+				this.byteBuffer.clear();
+				sb.reverse();
+				if(this.observer != null)
+				{
+
+					Object dto = this.dataFactory.createDTO(sb.toString());
+					if(dto != null)
+					{
+						this.observer.update(dto);
+					}
+				}
+			}
+		}
 	}
 
 	@Override
 	public void attachObserver(ISerialDataObserver observer)
 	{
-		// TODO Auto-generated method stub
-
+		this.observer = observer;
+		
 	}
 
 }
